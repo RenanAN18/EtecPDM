22bdfda503ed15a46ffa84f87796084913bb1af9
RenanAN18 <renanaugustonunes4@gmail.com>
2024-09-21 11:21:08 -0300

    Add files via upload

---
+++ ETECPDM/src/A07_FuncoesMatematicas.kt
@@ -0,0 +1,19 @@
+import kotlin.math.*
+
+//Aula 7: Funções Matemáticas
+
+fun main(){
+    println(max(5, 10)) //Pega o maior número
+    println(min(5,10)) //Pega o menor número
+    println(sqrt(49.0))//Exige que seja float ou double, então .0 ou f,pq é de raiz quadrada
+
+    println(PI)
+    println(E)
+
+    println(round(35465.960000546876))
+    println(round(PI))
+    println(round(E))
+
+    val PI = 3.14159
+    println(PI)
+}
