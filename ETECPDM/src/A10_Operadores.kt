72c9065c839037dfc01396d9c75f2a7017e00e46
Renan <renanaugustonunes4@gmail.com>
2024-10-04 18:06:58 -0300

    Add files via upload

---
+++ ETECPDM/src/A10_Operadores.kt
@@ -0,0 +1,17 @@
+// Aula 10: Operadores de Comparação e Igualdade, Operadores Lógicos e Operadores de Intervalo
+fun main(){
+    //Operadores de Comparação e igualdade: >, <, >=, <=
+    var n = 7
+
+    if(n > 5) println("$n é maior que 5")
+    if(n < 9) println("$n é menor que 9")
+    if(n >= 6) println("$n é maior ou igual a 6")
+    if(n <= 7) println("$n é menor ou igual a 7")
+
+    // Operadores de igualdade: ==, !=, ! (negação)
+    if (n == 7) println("$n é igual a 7")
+    if (n != 9) println("$n é diferente de 9")
+    if (!(n == 9)) println("$n não é igual a 9")
+}
+
+// parisotto@gmail.com // assunto: Link do github // se precisar enviar por lá
